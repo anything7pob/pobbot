e99bce1a11ee6fcf1176bd1b0bf7c61fa034db0e
anything7pob <flamencojoe@hotmail.com>
2017-01-23 16:12:11 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import tweepy
 from time import sleep
 class TwitterAPI:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     """
     Class for accessing the Twitter API.
