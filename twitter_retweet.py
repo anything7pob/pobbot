f2e3697e34915227a359e5b039d33b8f0207913c
anything7pob <flamencojoe@hotmail.com>
2017-01-23 12:22:20 +0000

    Create twitter_retweet.py

---
+++ twitter_retweet.py
@@ -0,0 +1,33 @@
+import tweepy
+from time import sleep
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+for tweet in tweepy.Cursor(api.search, q='#Romford','#Chelmsford','Roofing').items():
+    try:
+        print('\nTweet by: @' + tweet.user.screen_name)
+
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        # Favorite the tweet
+        tweet.favorite()
+        print('Favorited the tweet')
+
+    if not tweet.user.following:
+        # Don't forget to indent
+        tweet.user.follow()
+        print('Followed the user')
+       
+        
+
+        sleep(3600)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
